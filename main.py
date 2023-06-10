bfd2b753e3b675a1f7835c6a6b9afd24f1b5ebdc
Danial Tavakoli <dnialtavakoli@gmail.com>
2023-06-10 17:58:17 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+def multiply(a, b):
+    return a * b
+
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    return a / b
+
+def subtract(a, b):
+    return a - b
+  
+result = multiply(2, 3)
+print(result)  # prints 6
+
+result = add(2, 3)
+print(result)  # prints 5
+
+result = divide(10, 2)
+print(result)  # prints 5.0
+
+result = subtract(10, 2)
+print(result)  # prints 8
